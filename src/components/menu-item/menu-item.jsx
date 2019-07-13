09d501e9a009e975ebe585154c6d345dfcd9f76d
manasi9307 <manasi.thakur@sjsu.edu>
2019-07-13 11:43:06 -0700

    styling of components

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+
+export const MenuItem = (props) => {
+    return (
+        <div className={`{props.section.size} menu-item`}>
+        <div className="background-image" style={{
+            backgroundImage:`url(${props.section.imageUrl})`
+        }}/>
+              <div className="content">
+              <h1 className="title">{props.section.title.toUpperCase()}</h1>
+              <span className="subtitle">Shop</span>
+            </div>
+        </div>
+    )
+}
