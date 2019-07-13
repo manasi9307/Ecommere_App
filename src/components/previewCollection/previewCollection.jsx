8364a1c2818d958bf6256190b76bb6fc01058132
manasi9307 <manasi.thakur@sjsu.edu>
2019-07-13 15:34:04 -0700

    Collection item

--- src/components/previewCollection/previewCollection.jsx
+++ src/components/previewCollection/previewCollection.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './previewCollection.scss';
+import { CollectionItem } from '../collectionItem/collectionItem';
 
 export const PreviewCollection = ({title,items}) => {
     return(
         <div className="collection-preview">
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
-            {items.filter((item,index) => index<4).map(item => (
-                <div key={item.id}>{item.name}</div>
+            {items.filter((item,index) => index<4).map(({id, ...OtherProperties}) => (
+                <CollectionItem key={id} {...OtherProperties} />
             ))}
             </div>
         </div>
