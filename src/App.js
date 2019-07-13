e5a6d77886401949057d760e72d1cae07b673a2d
manasi9307 <manasi.thakur@sjsu.edu>
2019-07-12 20:48:44 -0700

    homepage setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Homepage} from './homepage';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Homepage />
     </div>
   );
 }
