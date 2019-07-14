3fee55f6fe167fba1cb7822cf1d0ed6cdfbdee3a
manasi9307 <manasi.thakur@sjsu.edu>
2019-07-13 17:38:29 -0700

    Header component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {Homepage} from './pages/homepage/homepage';
 import {Switch,Route} from 'react-router-dom';
 import Shop from './pages/shop/shop';
-
+import {Header} from './components/header/header'
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
       <Route exact path='/' component={Homepage} />
       <Route path='/shop' component={Shop} />
---
+++ src/components/header/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link } from 'react-router-dom';
+import './header.scss';
+import {ReactComponent as Logo} from '../../assests/crown.svg'
+export const Header = () => {
+    return(
+ <div className="header">
+      <Link className="logo-container" to="/">
+       <Logo className="logo" /> 
+      </Link>
+      <div className="options">
+      <Link className="option" to="/shop">
+          SHOP
+      </Link>
+      <Link className="option" to="/shop">
+          CONTACT
+      </Link>
+     </div>
+    </div>
+    )
+}
