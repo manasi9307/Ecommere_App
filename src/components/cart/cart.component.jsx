4da68223fe1ef728bcd78aabb6a7f18b5588cbbb
manasi9307 <manasi.thakur@sjsu.edu>
2019-08-05 19:19:57 -0700

    recent commit

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import CartItem from '../cart-item/cart-item';
+import {connect} from 'react-redux';
 
 
 import {CustomButton} from '../custom-button/custom-button';
 import './cart.scss';
 
 
-const Cart = () => {
+const Cart = ({cartItems}) => {
     return(
         <div className="cart-dropdown">
          <div className="cart-items">
-
+         {
+             cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+              ))
+            
+         }
          </div>
          <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
@@ -17,4 +24,8 @@ const Cart = () => {
 }
 
 
-export default Cart;
+const mapsStateToProps = ({cart :{cartItems}}) => ({
+    cartItems:cartItems
+})
+
+export default connect(mapsStateToProps)(Cart);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,7 +4,7 @@ const existing = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 if(existing){
     return cartItems.map(cartItem =>
         cartItem.id === cartItemToAdd.id
-        ?{...cartItem, quantity:cartItem.quantity+1}
+        ?{...cartItem, quantity:cartItem.quantity + 1}
         :cartItem
         )
 }
